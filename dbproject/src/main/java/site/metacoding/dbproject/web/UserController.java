b4d39fb6246f573c69f89cf0a7e3199ea3947382
coehoan <coehoan@naver.com>
2022-03-07 17:14:03 +0900

    회원정보 수정 페이지 데이터랜더링 완료

--- dbproject/src/main/java/site/metacoding/dbproject/web/UserController.java
+++ dbproject/src/main/java/site/metacoding/dbproject/web/UserController.java
@@ -123,8 +123,8 @@ public class UserController {
     }
 
     // 유저정보수정 페이지(동적) - 로그인O
-    @GetMapping("/user/{id}/updateForm")
-    public String updateForm(@PathVariable Integer id) {
+    @GetMapping("/user/updateForm")
+    public String updateForm() {
         return "user/updateForm";
     }
 
